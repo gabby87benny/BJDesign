88d21aab30f6b7f21a6089c9a4a2b2be2403d4f8
Gabriel Benny <gabby87benny@gmail.com>
2020-04-07 14:05:07 -0500

    Initial commit on BJ design

---
+++ BJDesign/BJDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BJDesign
+//
+//  Created by Joseph Peter, Gabriel Benny Francis on 4/7/20.
+//  Copyright © 2020 Gabby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
